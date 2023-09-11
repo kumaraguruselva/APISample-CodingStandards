d7dc8ac9f57c68398a94e96ab9a605dadf6cbd81
Kumaraguru s <kumaraguru.s@mphasis.com>
2023-09-11 10:55:38 +0530

    declared value as null

--- API/Employee.cs
+++ API/Employee.cs
@@ -4,9 +4,10 @@ namespace API
 {
     public class Employee
     {
+        
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Mobile { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set ; }
+        public string? Mobile { get; set; }
     }
 }
